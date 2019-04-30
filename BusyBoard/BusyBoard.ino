44c3375bf10ef0b2426b5ad28d6be711bd9634c1
Albom <o.v.bogomaz1985@gmail.com>
2019-04-30 23:54:48 +0300

    first commit

---
+++ BusyBoard/BusyBoard.ino
@@ -0,0 +1,82 @@
+
+#include <Keypad.h>
+#include "melodies.h"
+
+int buzzerPin = P2_3;
+int bellPin = P1_7;
+
+byte rowPins[] = { P1_4, P1_5, P2_0, P2_1 };
+byte colPins[] = { P1_0, P1_1, P1_2, P1_3 };
+
+const PROGMEM char keys[4][4] = {
+  {'1', '4', '7', '*'},
+  {'2', '5', '8', '0'},
+  {'3', '6', '9', '#'},
+  {'A', 'B', 'C', 'D'}
+};
+
+void play(const char melody[]) {
+  int i = 0;
+  while (melody[i]) {
+    int noteDuration = 1000 / melody[i + 1];
+    if (melody[i] > 0) {
+      tone(buzzerPin, NOTES[melody[i] - 1], noteDuration);
+      delay(noteDuration);
+      noTone(buzzerPin);
+    } else {
+      delay(noteDuration);
+    }
+    i += 2;
+  }
+}
+
+
+Keypad kpd = Keypad(makeKeymap(keys), rowPins, colPins, 4, 4);
+
+void setup() {
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(bellPin, INPUT_PULLUP);
+}
+
+void loop() {
+  if (digitalRead(bellPin) == LOW) {
+    play(bell);
+  }
+
+  char key = kpd.getKey();
+  if (key) {
+    switch (key)
+    {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        if (key == '0') {
+          key += 10;
+        }
+        for (int i = 0; i < key - '0'; i++) {
+          int noteDuration = 1000 / 2;
+          tone(buzzerPin, scale[i], noteDuration);
+          delay(noteDuration);
+          noTone(buzzerPin);
+        }
+        break;
+      case 'A':
+        play(friendship);
+        break;
+      case 'B':
+        play(mamont);
+        break;
+      case 'C':
+        play(oldMcDonald);
+        break;
+    }
+  }
+
+}
---
+++ BusyBoard/melodies.h
@@ -0,0 +1,151 @@
+#include "pitches.h"
+
+#define DF 1
+#define DH 2
+#define DQ 4
+#define DE 8
+#define DHX 16
+
+#define NP -1
+#define NEND  0
+
+#define NC3 1
+#define NCS3 2
+#define ND3 3
+#define NDS3 4
+#define NE3 5
+#define NF3 6
+#define NFS3 7
+#define NG3 8
+#define NGS3 9
+#define NA3 10
+#define NAS3 11
+#define NB3 12
+#define NC4 13
+#define NCS4 14
+#define ND4 15
+#define NDS4 16
+#define NE4 17
+#define NF4 18
+#define NFS4 19
+#define NG4 20
+#define NGS4 21
+#define NA4 22
+#define NAS4 23
+#define NB4 24
+#define NC5 25
+#define NCS5 26
+#define ND5 27
+#define NDS5 28
+#define NE5 29
+#define NF5 30
+#define NFS5 31
+#define NG5 32
+#define NGS5 33
+#define NA5 34
+#define NAS5 35
+#define NB5 36
+#define NC6 37
+
+
+const PROGMEM int NOTES[] = {
+  NOTE_C3,
+  NOTE_CS3,
+  NOTE_D3,
+  NOTE_DS3,
+  NOTE_E3,
+  NOTE_F3,
+  NOTE_FS3,
+  NOTE_G3,
+  NOTE_GS3,
+  NOTE_A3,
+  NOTE_AS3,
+  NOTE_B3,
+  NOTE_C4,
+  NOTE_CS4,
+  NOTE_D4,
+  NOTE_DS4,
+  NOTE_E4,
+  NOTE_F4,
+  NOTE_FS4,
+  NOTE_G4,
+  NOTE_GS4,
+  NOTE_A4,
+  NOTE_AS4,
+  NOTE_B4,
+  NOTE_C5,
+  NOTE_CS5,
+  NOTE_D5,
+  NOTE_DS5,
+  NOTE_E5,
+  NOTE_F5,
+  NOTE_FS5,
+  NOTE_G5,
+  NOTE_GS5,
+  NOTE_A5,
+  NOTE_AS5,
+  NOTE_B5,
+  NOTE_C6
+};
+
+const PROGMEM int scale[] = {NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4, NOTE_C5, NOTE_G5, NOTE_C6};
+const PROGMEM char bell[] = {NC5, DHX, NF5, DHX, NEND};
+const PROGMEM char friendship[] = {
+  NA4, DH, ND4, DH,
+  ND4, DQ, NCS4, DQ, NE4, DH,
+  NAS4, DH, NE4, DH, NE4, DQ, ND4, DQ, NF4, DH,
+  NC5, DH, NFS4, DH, NA4, DQ, NG4, DQ, NG4, DH,
+  NF4, DQ, NE4, DQ, ND4, DQ, NC4, DQ, NA4, DQ, NP, DQ, NP, DH,
+  ND5, DQ, NA4, DQ, NGS4, DQ, NA4, DQ,
+  NC5, DH, NAS4, DH,
+  NC5, DQ, NG4, DQ, NFS4, DQ, NG4, DQ,
+  NAS4, DH, NA4, DH,
+  NAS4, DQ, NE4, DQ, NF4, DQ, NG4, DQ,
+  NG4, DQ, NF4, DQ, NE4, DQ, ND4, DQ,
+  ND4, DH, NCS4, DH, NA4, DH, NP, DH,
+  NAS4, DQ, NE4, DQ, NF4, DQ, NG4, DQ,
+  NG4, DQ, NF4, DQ, NE4, DQ, ND4, DQ,
+  ND4, DH, NCS4, DH, ND4, DH,
+  NEND
+};
+
+const PROGMEM char oldMcDonald[] = {
+  NF5, DQ, NF5, DQ, NF5, DQ, NC5, DQ,
+  ND5, DQ, ND5, DQ, NC5, DH,
+  NA5, DQ, NA5, DQ, NG5, DQ, NG5, DQ, NF5, DH, NP, DQ,
+  NC5, DQ, NF5, DQ, NF5, DQ, NF5, DQ, NC5, DQ,
+  ND5, DQ, ND5, DQ, NC5, DH,
+  NA5, DQ, NA5, DQ, NG5, DQ, NG5, DQ, NF5, DH, NP, DQ,
+  NC5, DE, NC5, DE, NF5, DQ, NF5, DQ, NF5, DQ,
+  NC5, DE, NC5, DE, NF5, DQ, NF5, DQ, NF5, DQ,
+  NF5, DE, NF5, DE, NC5, DQ, NF5, DE, NF5, DE, NC5, DQ,
+  NF5, DE, NC5, DE, NF5, DE, NC5, DE, NF5, DQ, NF5, DQ, NP, DQ,
+  NF5, DQ, NF5, DQ, NF5, DQ, NC5, DQ,
+  ND5, DQ, ND5, DQ, NC5, DH,
+  NA5, DQ, NA5, DQ, NG5, DQ, NG5, DQ, NF5, DH, NP, DQ,
+  NEND
+};
+
+const PROGMEM char mamont[] = {
+  ND4, DQ, NF4, DQ, NF4, DQ, NF4, DQ, NF4, DH, NG4, DH,
+  ND4, DQ, NF4, DQ, NF4, DQ, NF4, DQ, NF4, DH, NP, DQ,
+  NAS3, DQ, NG4, DQ, NG4, DQ, NG4, DQ, NG4, DH, NAS4, DH,
+  NA4, DQ, NG4, DQ, NF4, DQ, NF4, DQ, NF4, DH,  NP, DQ,
+  NAS3, DQ, NAS4, DQ, NA4, DQ, NAS4, DQ, NA4, DH, NG4, DH, NA4, DQ,
+  NG4, DH, NF4, DF, NP, DQ,
+  NF4, DQ, NA4, DQ, NC4, DQ, ND4, DQ, NDS4, DH,
+  NG4, DH, NF4, DQ, NDS4, DH, ND4, DF, NP, DQ,
+  ND4, DQ, NG4, DQ, NA4, DQ, NAS4, DQ, NA4, DH, NG4, DH,
+  ND4, DQ, NG4, DQ, NA4, DQ, NAS4, DQ, NA4, DH, NG4, DH,
+  ND4, DQ, NG4, DQ, NA4, DQ, NAS4, DQ, NA4, DH, NG4, DH,
+  ND4, DQ, NF4, DQ, NDS4, DQ, ND4, DQ, NF4, DH, NDS4, DH,
+  NG4, DQ, NC5, DQ, NAS4, DQ, NC5, DQ, NAS4, DH, NA4, DH,
+  NG4, DQ, NG4, DH, ND4, DF, NP, DQ,
+  ND4, DQ, ND4, DQ, NFS4, DQ, NA4, DQ, NC5, DH, NAS4, DH,
+  NA4, DQ, NC5, DH, NAS4, DF, NP, DQ,
+  NC4, DQ, NC5, DQ, NAS4, DQ, NC5, DQ, NAS4, DH, NA4, DH,
+  NG4, DQ, NG4, DH, ND4, DF, NP, DQ,
+  ND4, DQ, ND4, DQ, NFS4, DQ, NA4, DQ, NC5, DH, NAS4, DH,
+  NA4, DQ, NA4, DH, NG4, DF,
+  NEND
+};
